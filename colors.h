e1b45247c2da14fa8a40b7523ac1407cbfc0965f
evertonse <evertonse.junior@gmail.com>
2023-07-13 19:45:07 -0300

    which patches

---
+++ colors.h
@@ -0,0 +1,50 @@
+#define vscFront     "#D4D4D4"
+#define vscBack      "#1E1E1E",
+
+#define vscTabCurrent  "#1E1E1E"
+#define vscTabOther  "#2D2D2D"
+#define vscTabOutside  "#252526"
+
+#define vscLeftDark  "#252526"
+#define vscLeftMid  "#373737"
+#define vscLeftLight  "#636369"
+
+#define vscPopupFront  "#BBBBBB",
+#define vscPopupBack  "#272727",
+#define vscPopupHighlightBlue  "#004b72",
+#define vscPopupHighlightGray  "#343B41",
+
+#define vscSplitLight  "#898989",
+#define vscSplitDark  "#444444",
+#define vscSplitThumb  "#424242",
+
+#define vscCursorDarkDark  "#222222",
+#define vscCursorDark  "#51504F",
+#define vscCursorLight  "#AEAFAD",
+#define vscSelection  "#264F78",
+#define vscLineNumber  "#5A5A5A",
+
+#define vscDiffRedDark  "#4B1818",
+#define vscDiffRedLight  "#6F1313",
+#define vscDiffRedLightLight  "#FB0101",
+#define vscDiffGreenDark  "#373D29",
+#define vscDiffGreenLight  "#4B5632",
+#define vscSearchCurrent  "#515c6a",
+#define vscSearch  "#613315",
+#define vscGitAdded  "#81b88b",
+#define vscGitModified  "#e2c08d",
+#define vscGitDeleted  "#c74e39",
+#define vscGitRenamed  "#73c991",
+#define vscGitUntracked  "#73c991",
+#define vscGitIgnored  "#8c8c8c",
+#define vscGitStageModified  "#e2c08d",
+#define vscGitStageDeleted  "#c74e39",
+#define vscGitConflicting  "#e4676b",
+#define vscGitSubmodule  "#8db9e2",
+#define vscContext  "#404040",
+#define vscContextCurrent  "#707070",
+#define vscFoldBackground  "#202d39",
+
+#define vscGray  "#808080",
+#define vscViolet  "#646695",
+#define vscBlue  "#569CD6",

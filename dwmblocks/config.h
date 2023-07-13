4529f14937e384bd032d9e2eab5cbba335164232
evertonse <evertonse.junior@gmail.com>
2023-07-13 20:47:54 -0300

    yeah

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -24,12 +24,12 @@ static const Block blocks[] = {
 	// {"",	"sb-internet",	5,	4},
 	/* {"",	"sb-iplocate", 0,	27}, */
 	// {"",	"sb-help-icon",	0,	15},
-  {"", "date +\"%T\""},
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+  {" ", "date +\"%T\"", 1 ,0},
+	{" Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = "  ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:

ba210013b8ae04bd1062733e8291ebc146f5e8c3
evertonse <evertonse.junior@gmail.com>
2023-07-28 20:05:51 +0000

    Fri Jul 28 08:05:51 PM UTC 2023

--- dwm/config.h
+++ dwm/config.h
@@ -14,14 +14,16 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-//static const char *fonts[]          = { "monospace:size=10" };
+
 static const char *fonts[] = { 
-  "Lekton\\ Nerd\\ Font\\ Complete\\ Mono:pixelsize=11:antialias=true:autohint=true",
+  "mono:pixelsize=12:antialias=true:autohint=true",
+  "NotoColorEmoji:pixelsize=11:antialias=true:autohint=true", 
+  "JetBrainsMonoNF:pixelsize=11:antialias=true:autohint=true" 
+  "Lekton\\ Nerd\\ Font\\ Complete\\ Mono:pixelsize=12:antialias=true:autohint=true",
   "monospace:pixelsize=12:antialias=true:autohint=true",
   "Lekton\\ Nerd\\ Font\\ Complete\\ Mono.ttf:pixelsize=11:antialias=true:autohint=true",
   "Lekton Nerd Font Complete Mono.ttf:pixelsize=11:antialias=true:autohint=true",
   "JetbrainsMonoNerdFonts:pixelsize=11:antialias=true:autohint=true",
-  "NotoColorEmoji:pixelsize=11:antialias=true:autohint=true", 
 };
 static const char dmenufont[]       = "monospace:size=11";
 
@@ -58,6 +60,7 @@ static const char *colors[][3]      = {
 /* tagging */
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const char *tags[] = { "","1", "2", "3", "4", "5", "6", "7", "8"};
+// static const char *tags[] = { "'","1", "2", "3", "4", "5", "6", "7", "8"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -69,7 +72,7 @@ static const Rule rules[] = {
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Blueman", NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "Google Chrome", NULL,     NULL,     2,         0,          1,           0,        -1 },
+	{ "Google Chrome", NULL,     NULL,     1 << 7,         0,          1,           0,   -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0} }; /* represents the icon as an array of vertices */
@@ -84,9 +87,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[T]",      tile },    /* first entry is default */
-	{ "[F]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "  [T]  ", tile },    /* first entry is default */
+	{ "  [F]  ", NULL },    /* no layout function means floating behavior */
+	{ "  [M]  ", monocle},
 };
 
 /* key definitions */

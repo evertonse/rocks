776122d45d0ae9e2ee1719e3566fbc17c203788d
evertonse <evertonse.junior@gmail.com>
2023-07-28 19:11:30 -0300

    exlcude

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ./dmenu/ && sudo make install && cd ..
-cd ./dwm/ && sudo make install && cd ..
-cd ./dwmblocks/ && sudo make clean install && cd ..
-cd ./st/ && sudo make install && cd ..
-cd ./slstatus/ && sudo make install && cd ..
